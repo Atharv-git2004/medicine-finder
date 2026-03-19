547efb376d4b6b9dd8fb9138ddf863d190ba8f23
Aswinchandran-6649 <aswinchandran6649@gmail.com>
2026-03-19 21:23:59 +0530

    server created mongodb connected

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.connection_string)
+.then(() => {
+    console.log("Database connected successfully");
+})
+.catch((err) => {
+    console.log("Database connection error:", err);
+});
